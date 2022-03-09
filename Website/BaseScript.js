14ef2bec989aefd58ac8c368881903151fa0ec12
Christian <christian-hahn1@gmx.net>
2022-03-09 14:41:56 +0100

    Intend

--- Website/BaseScript.js
+++ Website/BaseScript.js
@@ -6,9 +6,9 @@
     var _boxever_settings = {
         client_key: '[YOUR_CLIENTKEY]', // Replace with your client key
         target: 'https://api.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
-        cookie_domain: '*.[YOUR_DOMAIN].com/' // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com",
-		pointOfSale: "[YOUR_POS]",  // Replace with your POS
-		web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net",
+        cookie_domain: '*.[YOUR_DOMAIN].com/', // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
+	pointOfSale: "[YOUR_POS]",  // Replace with your POS
+	web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net",
     };
 	
     // Import the Boxever library asynchronously 
